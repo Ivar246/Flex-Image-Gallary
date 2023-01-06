1ffbe19d80dc171a1ad495b3855fc6f1d48c0c81
IVAR AHTSERHS <ravistha869@gmail.com>
2023-01-06 10:35:35 +0545

    bug fixed

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const panels = document.querySelectorAll(".panel");
+
 function addFlexAnimation() {
   this.classList.toggle("open");
 }
 
 function toggleActive(e) {
-  if (e.propertyName === "flex-grow") this.classList.toggle("open-active");
+  if (e.propertyName.includes("flex-grow"))
+    this.classList.toggle("open-active");
 }
 
 panels.forEach((panel) => panel.addEventListener("click", addFlexAnimation));
